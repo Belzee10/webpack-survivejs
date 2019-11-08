15d90081f7cf641a50f51bda3b6872e6490cedd6
Ivan <belzee92@gmail.com>
2019-11-07 23:11:39 -0500

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const WebpackNotifierPlugin = require("webpack-notifier");
 
 module.exports = {
+  devtool: "cheap-module-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       title: "Webpack demo"
-    })
-  ]
+    }),
+    new ErrorOverlayPlugin(),
+    new FriendlyErrorsWebpackPlugin(),
+    new WebpackNotifierPlugin()
+  ],
+  devServer: {
+    stats: "errors-only",
+    host: process.env.HOST,
+    port: process.env.PORT,
+    // open: true,
+    overlay: true
+  }
 };
